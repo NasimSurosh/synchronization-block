80cb38abfdf1ce0592294de5d37fad3545c693dc
NasimSursh <suroshzahiri@gmail.com>
2024-02-10 11:46:19 +0100

    synchronizd with excercises

---
+++ src/explination/Application.java
@@ -0,0 +1,13 @@
+package explination;
+
+public class Application {
+
+  public static void main(String[] args) {
+    
+    Calculation calc = new Calculation();
+    
+    calc.main();
+
+  }
+
+}
